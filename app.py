571a994ed3fac48fe6d8b331f51ad0b397b9d27f
JosephInAfrica <lochleee@gmail.com>
2018-09-07 17:50:59 +0800

    0.1

--- app.py
+++ app.py
@@ -22,37 +22,25 @@ headers={'accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/w
 "Accept-Encoding":"gzip,deflate",
 "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"}
 
-
 class noForm(FlaskForm):
 	no=IntegerField('how many recent days do you want the graph to cover?',validators=[Required()])
 	submit=SubmitField('Submit')
 
-
-
 @app.route('/',methods=['GET','POST'])
 def index():
 	form=noForm()
-	a=updater('database.db')
-	data=a.fetch_all()
-
+	handler=DataHandler('database.db')
+	data=handler.available
+	
 	if form.validate_on_submit():
 		days=form.no.data
 		return render_template('index.html',chart=data[-days:],form=form)
 	return render_template("index.html",chart=data[-1000:],form=form)
 
-
 def url_to_name(url):
 	url=re.sub('[:/\.]|html|','',url)
 	url=re.sub('httpquotesmoney163comtradelsjysj','',url)
 	return datetime.today().strftime('%Y-%m-%d')+url
-
- 
-# def render(chart):
-# 	with open(os.path.join(templates_dir,'index.html'),'r') as file:
-# 		temp=Template(file.read())
-# 		result=temp.render(chart=chart)
-# 		print(result)
-# 		return result
 
 class parser(object):
 	headers={'accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
@@ -94,24 +82,22 @@ class parser(object):
 
 	def get_chart(self):
 		self.chart=[[row[0],row[-1]] for row in self.matrix]
- 
 
-class updater(object):
+class DataHandler(object):
 	shz=parser(shenzhen)
 	ssh=parser(shanghai)
 
 	def __init__(self,database):
 		self.database=database
 		self.connect()
-		self.fetch_available()
+		self.fetch_available_dates()
 		self.update()
-		self.conn.close()
+		# self.conn.close()
 
 	def update(self):
 		shzdict=dict(self.shz.chart)
 		sshdict=dict(self.ssh.chart)
 		combined={}
-
 		for i in shzdict:
 			try:
 				combined[i]=sshdict[i]+shzdict[i]
@@ -121,14 +107,16 @@ class updater(object):
 		for i in sorted(combined.keys(),reverse=True):
 			datei=datetime.strptime(str(i),'%Y%m%d')
 			date_in_string=datetime.strftime(datei,'%Y-%m-%d')
-			if datei in self.available_dates:
+			if datei in self.recorded_dates:
 				break
-			# print(i,combined[i],)
-			self.cursor.execute('insert into datas values (?,?)',(date_in_string,combined[i]))
-			self.conn.commit()
-
+			try:
+				self.cursor.execute('insert into datas values (?,?)',(date_in_string,combined[i]))
+			except BaseException as e:
+				print(e)
 			print(date_in_string,combined[i],'newly inserted')
-		# self.conn.close()
+		self.conn.commit()
+		self.available=[i for i in self.cursor.execute('select * from datas order by date')]
+		self.conn.close()
 
 	def connect(self):
 		conn=sqlite3.connect(self.database)
@@ -136,13 +124,8 @@ class updater(object):
 		self.conn=conn
 		self.cursor=conn.cursor()
 
-	def fetch_available(self):
-		self.available_dates=[datetime.strptime(i[0],'%Y-%m-%d') for i in list(self.cursor.execute('select date from datas'))]
-	# this func could combine with the downside one.
-
-	def fetch_all(self):
-		all=[i for i in self.cursor.execute('select * from datas order by date')]
-		self.available=all
+	def fetch_available_dates(self):
+		self.recorded_dates=[datetime.strptime(i[0],'%Y-%m-%d') for i in list(self.cursor.execute('select date from datas'))]
 
 if __name__=='__main__':
 	app.run(debug=True)
