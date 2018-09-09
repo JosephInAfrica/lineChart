1381b17bf0ac5ebd222c529ce9fd62fa343b9a3d
JosephInAfrica <lochleee@gmail.com>
2018-09-09 15:18:35 +0800

    commit

--- app.py
+++ app.py
@@ -23,109 +23,108 @@ headers={'accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/w
 "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"}
 
 class noForm(FlaskForm):
-	no=IntegerField('how many recent days do you want the graph to cover?',validators=[Required()])
-	submit=SubmitField('Submit')
+    no=IntegerField('how many recent days do you want the graph to cover?',validators=[Required()])
+    submit=SubmitField('Submit')
 
 @app.route('/',methods=['GET','POST'])
 def index():
-	form=noForm()
-	handler=DataHandler('database.db')
-	data=handler.available
-	
-	if form.validate_on_submit():
-		days=form.no.data
-		return render_template('index.html',chart=data[-days:],form=form)
-	return render_template("index.html",chart=data[-1000:],form=form)
+    form=noForm()
+    handler=DataHandler('database.db')
+    data=handler.available
+    if form.validate_on_submit():
+        days=form.no.data
+        return render_template('index.html',chart=data[-days:],form=form)
+    return render_template("index.html",chart=data[-1000:],form=form)
 
 def url_to_name(url):
-	url=re.sub('[:/\.]|html|','',url)
-	url=re.sub('httpquotesmoney163comtradelsjysj','',url)
-	return datetime.today().strftime('%Y-%m-%d')+url
+    url=re.sub('[:/\.]|html|','',url)
+    url=re.sub('httpquotesmoney163comtradelsjysj','',url)
+    return datetime.today().strftime('%Y-%m-%d')+url
 
 class parser(object):
-	headers={'accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-	"Accept-Encoding":"gzip,deflate",
-	"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"}
+    headers={'accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding":"gzip,deflate",
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"}
 
-	def __init__(self,url):
-		self.url=url
-		self.get_soup()
-		self.get_rows()
-		self.get_matrix()
-		self.get_chart()
+    def __init__(self,url):
+        self.url=url
+        self.get_soup()
+        self.get_rows()
+        self.get_matrix()
+        self.get_chart()
 
-	def get_soup(self):
-		url=self.url
-		try:
-			with open(os.path.join(cachedir,url_to_name(url)+".html"),'rb') as file:
-				soup=Soup(file,"lxml")
-				print('reading from cahce...\n')
-		except:
-			print("we have to download...")
-			r=requests.get(url,headers=self.headers)
-			print('done downloading')
-			with open(os.path.join(cachedir,url_to_name(url)+".html"),'wb') as file:
-				file.write(r.content)
-			soup=Soup(r.content,"lxml")
- 		self.soup=soup
+    def get_soup(self):
+        url=self.url
+        try:
+            with open(os.path.join(cachedir,url_to_name(url)+".html"),'rb') as file:
+                soup=Soup(file,"lxml")
+                print('reading from cahce...\n')
+        except:
+            print("we have to download...")
+            r=requests.get(url,headers=self.headers)
+            print('done downloading')
+            with open(os.path.join(cachedir,url_to_name(url)+".html"),'wb') as file:
+                file.write(r.content)
+            soup=Soup(r.content,"lxml")
+        self.soup=soup
 
-	def get_rows(self):
-		self.rows=self.soup.select("div.inner_box > table > tr")
+    def get_rows(self):
+        self.rows=self.soup.select("div.inner_box > table > tr")
 
-	def get_matrix(self):
-		def treat_no(no):
-			if '.' not in no:
-				return int(no)
-			return float(re.sub(',','',no))
-		tds=[row.select('td') for row in self.rows]
-		self.matrix=[[treat_no(i.contents[0]) for i in td] for td in tds]
+    def get_matrix(self):
+        def treat_no(no):
+            if '.' not in no:
+                return int(no)
+            return float(re.sub(',','',no))
+        tds=[row.select('td') for row in self.rows]
+        self.matrix=[[treat_no(i.contents[0]) for i in td] for td in tds]
 
-	def get_chart(self):
-		self.chart=[[row[0],row[-1]] for row in self.matrix]
+    def get_chart(self):
+        self.chart=[[row[0],row[-1]] for row in self.matrix]
 
 class DataHandler(object):
-	shz=parser(shenzhen)
-	ssh=parser(shanghai)
+    shz=parser(shenzhen)
+    ssh=parser(shanghai)
 
-	def __init__(self,database):
-		self.database=database
-		self.connect()
-		self.fetch_available_dates()
-		self.update()
-		# self.conn.close()
+    def __init__(self,database):
+        self.database=database
+        self.connect()
+        self.fetch_available_dates()
+        self.update()
+        # self.conn.close()
 
-	def update(self):
-		shzdict=dict(self.shz.chart)
-		sshdict=dict(self.ssh.chart)
-		combined={}
-		for i in shzdict:
-			try:
-				combined[i]=sshdict[i]+shzdict[i]
-			except BaseException as e:
-				print (e)
-		self.today=combined
-		for i in sorted(combined.keys(),reverse=True):
-			datei=datetime.strptime(str(i),'%Y%m%d')
-			date_in_string=datetime.strftime(datei,'%Y-%m-%d')
-			if datei in self.recorded_dates:
-				break
-			try:
-				self.cursor.execute('insert into datas values (?,?)',(date_in_string,combined[i]))
-			except BaseException as e:
-				print(e)
-			print(date_in_string,combined[i],'newly inserted')
-		self.conn.commit()
-		self.available=[i for i in self.cursor.execute('select * from datas order by date')]
-		self.conn.close()
+    def update(self):
+        shzdict=dict(self.shz.chart)
+        sshdict=dict(self.ssh.chart)
+        combined={}
+        for i in shzdict:
+            try:
+                combined[i]=sshdict[i]+shzdict[i]
+            except BaseException as e:
+                print (e)
+        self.today=combined
+        for i in sorted(combined.keys(),reverse=True):
+            datei=datetime.strptime(str(i),'%Y%m%d')
+            date_in_string=datetime.strftime(datei,'%Y-%m-%d')
+            if datei in self.recorded_dates:
+                break
+            try:
+                self.cursor.execute('insert into datas values (?,?)',(date_in_string,combined[i]))
+            except BaseException as e:
+                print(e)
+            print(date_in_string,combined[i],'newly inserted')
+        self.conn.commit()
+        self.available=[i for i in self.cursor.execute('select * from datas order by date')]
+        self.conn.close()
 
-	def connect(self):
-		conn=sqlite3.connect(self.database)
-		c=conn.cursor()
-		self.conn=conn
-		self.cursor=conn.cursor()
+    def connect(self):
+        conn=sqlite3.connect(self.database)
+        c=conn.cursor()
+        self.conn=conn
+        self.cursor=conn.cursor()
 
-	def fetch_available_dates(self):
-		self.recorded_dates=[datetime.strptime(i[0],'%Y-%m-%d') for i in list(self.cursor.execute('select date from datas'))]
+    def fetch_available_dates(self):
+        self.recorded_dates=[datetime.strptime(i[0],'%Y-%m-%d') for i in list(self.cursor.execute('select date from datas'))]
 
 if __name__=='__main__':
-	app.run(debug=True)
+    app.run(debug=True)
